a0b629621f37aad947524e90c01b5ef14a19ab77
Rafael Lopez <lpz.raf@gmail.com>
2019-08-30 13:08:12 -0400

    Updates

---
+++ precache-manifest.c5e2012bba585fbaa5b0081859cd2967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4cda829c03310071fe233832f27b7b",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "b90f0117790f8f7c30f6",
+    "url": "/react-todo-app/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "5ae9ce24d63aa9afb5a1",
+    "url": "/react-todo-app/static/js/2.51f71208.chunk.js"
+  },
+  {
+    "revision": "b90f0117790f8f7c30f6",
+    "url": "/react-todo-app/static/js/main.5d4142f6.chunk.js"
+  },
+  {
+    "revision": "607c3a111f56b4fedbec",
+    "url": "/react-todo-app/static/js/runtime~main.bad4d63e.js"
+  }
+]);
